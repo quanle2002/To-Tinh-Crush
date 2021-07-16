6a691074b1a400d1dfaba4786c633d703a283b73
quanle2002 <87535420+quanle2002@users.noreply.github.com>
2021-07-16 21:35:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Lại là Quân đây hihi không biết văn vẻ gì mấy`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Mình đã thích cậu từ cái nhìn đầu tiên',
+    desc: 'Phải chăng mình đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Mình cũng thích cậu lắm <33',
+    btnNo: 'Không, bạn đừng chêu mình :3',
+    question:'Mình thích cậu hihi',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Mình yêu nhau đi <33333333',
+    mess: 'Em biết mà 🥰. Yêu anh lắm hihi 😘😘',
+    messDesc: 'Còn cái nịt',
+    btnAccept: 'Hihi <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
